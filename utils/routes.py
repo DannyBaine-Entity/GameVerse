ad81b2bccd0b94d3d5fc4289f3edc488eba6234c
DannyBaine-Entity <apololadanieltolu@gmail.com>
2024-09-16 19:54:53 +0100

    Delete unnecessary files and packages

--- utils/routes.py
+++ utils/routes.py
@@ -1,4 +1,6 @@
+from flask import flash
 from flask import render_template, request, redirect, url_for, session
+from sqlalchemy.exc import IntegrityError
 from .app import app
 from .database import db
 from .models import User
@@ -7,8 +9,6 @@ import bcrypt
 @app.route('/')
 def index():
     return render_template("landing.html")
-
-from flask import flash
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -19,7 +19,7 @@ def login():
         if user and bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
             return redirect(url_for('home'))  # Redirects user to home page
         else:
-            flash('Invalid email or password. Please try again.', 'danger')
+            flash('Invalid email or password. Please try again.', 'login')
     return render_template('login.html')
 
 
@@ -29,34 +29,44 @@ def register():
         email = request.form.get('email')
         username = request.form.get('username')
         password = request.form.get('password')
+        confirm_password = request.form.get('confirm_password')  # Get the confirm password field
 
-        user = User.query.filter_by(email=email, username=username).first()
+        # Check if the passwords match
+        if password != confirm_password:
+            flash('Passwords do not match. Please try again.', 'register')
+            return redirect(url_for('register'))  # Redirect back to the registration page
 
-        if user:
-            flash('User already exists. Please choose a different one.', 'danger')
+        user = User.query.filter((User.email == email) | (User.username == username)).first()
+
+        if user is not None:
+            flash('User already exists. Please choose a different one.', 'login')
             return redirect(url_for('login'))
 
         # Password conditions
         if len(password) < 8:
-            flash('Password must be at least 8 characters long.', 'danger')
+            flash('Password must be at least 8 characters long.', 'register')
             return redirect(url_for('register'))
         elif not any(char.isupper() for char in password):
-            flash('Password must contain at least one uppercase letter.', 'danger')
+            flash('Password must contain at least one uppercase letter.', 'register')
             return redirect(url_for('register'))
         elif not any(char.islower() for char in password):
-            flash('Password must contain at least one lowercase letter.', 'danger')
+            flash('Password must contain at least one lowercase letter.', 'register')
             return redirect(url_for('register'))
         elif not any(char.isdigit() for char in password):
-            flash('Password must contain at least one numeric digit.', 'danger')
+            flash('Password must contain at least one numeric digit.', 'register')
             return redirect(url_for('register'))
         elif not any(char.isalnum() or char in '!@#$%^&*()-_=+[]{}|;:\'",.<>?/`~' for char in password):
-            flash('Password must contain at least one special character.', 'danger')
+            flash('Password must contain at least one special character.', 'register')
             return redirect(url_for('register'))
 
         hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
         new_user = User(email=email, username=username, password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+        except IntegrityError:
+            db.session.rollback()
+            flash('User already exists. Please choose a different one.', 'login')
         return redirect(url_for('login'))  # Redirects user to login page
     return render_template("register.html")
 
